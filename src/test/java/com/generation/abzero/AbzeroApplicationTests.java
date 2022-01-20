ff2588f0709dd36ad77278dc0e14e63a288cea21
electra995 <92882462+electra995@users.noreply.github.com>
2022-01-20 20:08:44 +0100

    Add files via upload

---
+++ src/test/java/com/generation/abzero/AbzeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.abzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbzeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
